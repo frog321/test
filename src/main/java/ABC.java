f1dd09eab66e60f6e3479ce0a3626cd1c8072880
wogo <wgosztyla@gmail.com>
2019-04-13 22:27:59 +0200

    aa

---
+++ src/main/java/ABC.java
@@ -0,0 +1,31 @@
+public class ABC {
+
+    public static void main(String[] args) {
+        System.out.println("test 1 br");
+        ABC abc = new ABC();
+        abc.dummy();
+
+        int i = 0;
+
+        while (i < 1000) {
+            try {
+                Thread.sleep(1000);
+                System.out.println("Slept ... in branch  " + "["+i+"]");
+                i++;
+                doSomething();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void doSomething() {
+        System.out.println("Doing additional work ...");
+    }
+
+    public boolean dummy() {
+        return true;
+    }
+
+}
