a1894134b5359160595766c24ea51ab88bd2c2b6
wogo <wgosztyla@gmail.com>
2019-04-13 22:22:35 +0200

    aa

---
+++ src/test/java/Test1.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1 {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        ABC tester = new ABC(); // MyClass is tested
+
+        // assert statements
+        assertTrue(tester.dummy());
+    }
+}
